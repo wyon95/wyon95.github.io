edc097f977fd3199dae981bacac90bd1052807e0
choiwonseok9 <abdnjs@naver.com>
2021-08-27 11:30:34 +0900

    454
    4445

--- main/CoronaCore/01_code/submenu_2/js/utils/Set_Date.js
+++ main/CoronaCore/01_code/submenu_2/js/utils/Set_Date.js
@@ -1,5 +1,8 @@
 let start = '2020-03-03'
 let end = '2021-06-09'
+
+const date_array = [];              // 해당 필요부분 넣을 배열 생성
+
 /*function requestApiAndGetData() {
   return fetch('https://coronaproject.herokuapp.com/api/covidDate').then(res => res.json()).then(obj => {
     return obj.data
@@ -11,7 +14,7 @@ function calculateDateArray(end){
   let dateDiff = Math.ceil((edt.getTime()-sdt.getTime())/(1000*3600*24));
 
   let Year,Month,Day;                 // 각 날짜별 날짜 생성
-  const date_array = [];              // 해당 필요부분 넣을 배열 생성
+  
   for(let i=0;i<dateDiff;i++)              // 2020년 3월 4일부터 마지막 데이터 csv까지의 날짜의 차이에서 +1 한 값이 150 (ex 현재는 7월 31일까지)
   {
     sdt.setDate(sdt.getDate()+1)        // 3월 4일 계산 후 하루씩 증가
@@ -25,7 +28,8 @@ function calculateDateArray(end){
 
 async function Set_Date(){
   //let data = await requestApiAndGetData();
-  let date_array = await calculateDateArray(data);
+  //let date_array = await calculateDateArray(data);
+  
   return date_array;
 }
 
