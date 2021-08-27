e93ba967b1d86db2685d332f8d80ae53b57920dd
choiwonseok9 <abdnjs@naver.com>
2021-08-28 00:10:49 +0900

    1
    1

--- main/CoronaCore/01_code/submenu_2/js/utils/Data.js
+++ main/CoronaCore/01_code/submenu_2/js/utils/Data.js
@@ -1,4 +1,4 @@
 //const url = 'http://coronacore.site/main/CoronaCore/01_code/submenu_2/Data/Date/'
-const url = 'https://wyon95.github.io/main/CoronaCore/01_code/submenu_2/Data/Date/'
+const url = 'http://wyon95.github.io/main/CoronaCore/01_code/submenu_2/Data/Date/'
 
 export {url};
